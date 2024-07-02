08039c5ad2c4e7bdb426adfa5252eddd81599a8f
kingkrazee <kaydenhendrick04@gmail.com>
2024-07-02 14:03:49 +0200

    about

--- src/store/index.js
+++ src/store/index.js
@@ -2,13 +2,24 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    aboutMe: null
   },
   getters: {
   },
   mutations: {
+    setAboutMe(state, info) {
+      state.aboutMe = info
   },
-  actions: {
-  },
+},
+actions: {
+  async getAboutMe(context){
+    let fetchedData = await fetch('https://kingkrazee.github.io/first-api/data/data.json');
+    let oor = await fetchedData.json();
+    console.log(oor);
+    let {AboutMe} = oor
+    context.commit('setAboutMe',AboutMe)
+  }
+},
   modules: {
   }
 })
